82b03868c2a736486f66a86d896408f68e016757
akshay bhat <akshayubhat@gmail.com>
2016-09-26 03:48:18 -0400

    fixed tests

--- tests/helpers.py
+++ tests/helpers.py
@@ -4,12 +4,12 @@ import subprocess
 
 def tree_correct(path):
     with open("/tmp/output.txt", "wb") as out, open("/tmp/err.txt", "wb") as err:
-        subprocess.Popen("tree {}".format(path), stdout=out, stderr=err)
+        subprocess.Popen(["tree",path], stdout=out, stderr=err)
     return open('/tmp/output.txt').read()
 
 
 def pytree(path):
     with open("/tmp/pytree.txt", "wb") as out, open("/tmp/pyerr.txt", "wb") as err:
-        subprocess.Popen("./pytree.py {}".format(path), stdout=out, stderr=err)
+        subprocess.Popen(["./pytree.py",path], stdout=out, stderr=err)
     return open('/tmp/pytree.txt').read()
 
