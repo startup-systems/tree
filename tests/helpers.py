cc7433b05a33b9f291536bec58c8a60bafe9c645
akshay bhat <akshayubhat@gmail.com>
2016-09-26 03:38:51 -0400

    fixed tests

--- tests/helpers.py
+++ tests/helpers.py
@@ -3,10 +3,10 @@ import subprocess
 
 
 def tree_correct(path):
-    subprocess.run(['tree', path, '> output.txt'])
+    subprocess.run(['tree', path, '> /tmp/output.txt'])
     return open('output.txt').read()
 
 
 def pytree(path):
-    subprocess.run(['./pytree.py', path, '> pytree.txt'])
+    subprocess.run(['./pytree.py', path, '> /tmp/pytree.txt'])
     return open('pytree.txt').read()
