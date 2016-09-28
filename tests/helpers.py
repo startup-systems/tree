ee72ed9cc69b621c36e8ec51008829b2e7c2b4f8
Aidan Feldman <alf239@cornell.edu>
2016-09-28 15:11:52 +0000

    check for number of directories and number of files

--- tests/helpers.py
+++ tests/helpers.py
@@ -2,8 +2,8 @@ import subprocess
 import sys
 
 
-def run_and_capture(cmd, path):
-    result = subprocess.run([cmd, path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+def run_and_capture(args):
+    result = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 
     # handle failures
     print(result.stderr, file=sys.stderr)
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,8 +1,31 @@
 import helpers
+import pytest
 
 
-def test_compare():
+@pytest.mark.score(5)
+def test_num_directories():
     path = 'examples'
-    expected = helpers.run_and_capture('tree', path)
-    actual = helpers.run_and_capture('./pytree.py', path)
+    output = helpers.run_and_capture(['./pytree.py', path])
+    assert "3 directories" in output
+
+
+@pytest.mark.score(5)
+def test_num_files():
+    path = 'examples'
+    output = helpers.run_and_capture(['./pytree.py', path])
+    assert "4 files" in output
+
+
+@pytest.mark.score(40)
+def test_with_path():
+    path = 'examples'
+    expected = helpers.run_and_capture(['tree', path])
+    actual = helpers.run_and_capture(['./pytree.py', path])
+    assert expected == actual
+
+
+@pytest.mark.score(40)
+def test_without_path():
+    expected = helpers.run_and_capture(['tree'])
+    actual = helpers.run_and_capture(['./pytree.py'])
     assert expected == actual
