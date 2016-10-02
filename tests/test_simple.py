fe32c582fe6ee25698c164b06bd9b3cc2b829bf2
Aidan Feldman <alf239@cornell.edu>
2016-10-02 05:01:43 +0000

    add check for the right filenames

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,5 +1,6 @@
 import helpers
 import pytest
+import re
 
 
 @pytest.mark.score(5)
@@ -16,7 +17,15 @@ def test_num_files():
     assert "4 files" in output
 
 
-@pytest.mark.score(40)
+@pytest.mark.score(20)
+def test_file_dir_names():
+    path = 'examples'
+    output = helpers.run_and_capture(['./pytree.py', path])
+    names = re.findall('[a-z]+(?:\.[a-z]+)?', output)
+    assert names == ['examples', 'simple', 'another', 'bites', 'dust', 'dot.jpg', 'the', 'one.txt', 'directories', 'files']
+
+
+@pytest.mark.score(30)
 def test_with_path():
     path = 'examples'
     expected = helpers.run_and_capture(['tree', path])
@@ -24,7 +33,7 @@ def test_with_path():
     assert expected == actual
 
 
-@pytest.mark.score(40)
+@pytest.mark.score(30)
 def test_without_path():
     expected = helpers.run_and_capture(['tree'])
     actual = helpers.run_and_capture(['./pytree.py'])
