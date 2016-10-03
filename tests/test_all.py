74d6a7e60fcfc89e5228ba836f5931fab67ab5a0
Aidan Feldman <alf239@cornell.edu>
2016-10-03 03:00:49 +0000

    run the test without path from within the examples directory
    This will help to ensure there aren't funky filenames that make sorting confusing.

--- tests/test_all.py
+++ tests/test_all.py
@@ -3,7 +3,8 @@ import pytest
 
 
 @pytest.mark.score(5)
-def test_without_path():
+def test_without_path(monkeypatch):
+    monkeypatch.chdir('examples')
     expected = helpers.run_and_capture(['tree'])
-    actual = helpers.run_and_capture(['./pytree.py'])
+    actual = helpers.run_and_capture(['../pytree.py'])
     assert expected == actual
